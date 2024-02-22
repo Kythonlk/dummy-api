bdd500b8925742abca7e5cea59d64be221bdcb40
kyhtonllk <kythonlk@gmail.com>
2024-02-22 10:24:02 +0400

    Update cores

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3030;
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// app.use(bodyParser.json());
 
 const db = new sqlite3.Database(":memory:");
 
